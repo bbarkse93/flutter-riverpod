b72012e05de75349e96a3b63621dcf4402e70d36
bbarkse93 <bbarkse93@gmail.com>
2023-10-18 14:05:00 +0900

    글쓰기

--- lib/data/repository/post_repository.dart
+++ lib/data/repository/post_repository.dart
@@ -1,8 +1,10 @@
 // View -> Provider(전역 Provider, View Model) -> Repository
 import 'package:dio/dio.dart';
 import 'package:flutter_blog/_core/constants/http.dart';
+import 'package:flutter_blog/data/dto/post_request.dart';
 import 'package:flutter_blog/data/dto/response_dto.dart';
 import 'package:flutter_blog/data/model/post.dart';
+import 'package:logger/logger.dart';
 
 class PostRepository {
   Future<ResponseDTO> fetchPostList(String jwt) async {
@@ -24,6 +26,34 @@ class PostRepository {
     } catch (e) {
       // 200이외의 상태코드는 무조건 catch로 감 (통신은 무조건 try-catch)
       return ResponseDTO(-1, "게시글 목록 불러오기 실패", null);
+    }
+  }
+
+  Future<ResponseDTO> fetchPost(String jwt, PostSaveReqDTO postSaveReqDTO) async {
+    try {
+      // 1. 통신
+      final response = await dio.post(
+        "/post",
+        data: postSaveReqDTO.toJson(),
+        options: Options(headers: {"Authorization": "${jwt}"}),
+      );
+
+      // 2. 파싱
+      ResponseDTO responseDTO = ResponseDTO.fromJson(response.data);
+      Logger().d("파싱: ${responseDTO.msg}");
+
+      // 3. ResponseDTO의 data파싱
+      Post post = Post.fromJson(responseDTO.data);
+      // responseDTO.data = User.fromJson(responseDTO.data);
+
+      // 4. 파싱된 데이터를 다시 공통 dto로 덮어씌우기
+      responseDTO.data = post;
+      Logger().d("덮어 씌워짐");
+
+      return responseDTO;
+    } catch (e) {
+      // 200이외의 상태코드는 무조건 catch로 감 (통신은 무조건 try-catch)
+      return ResponseDTO(-1, "게시글 작성 실패", null);
     }
   }
 }
--- lib/ui/pages/post/list_page/post_list_view_model.dart
+++ lib/ui/pages/post/list_page/post_list_view_model.dart
@@ -1,8 +1,11 @@
 // 1. 창고 데이터
+import 'package:flutter/material.dart';
+import 'package:flutter_blog/data/dto/post_request.dart';
 import 'package:flutter_blog/data/dto/response_dto.dart';
 import 'package:flutter_blog/data/model/post.dart';
 import 'package:flutter_blog/data/provider/session_provider.dart';
 import 'package:flutter_blog/data/repository/post_repository.dart';
+import 'package:flutter_blog/main.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class PostListModel {
@@ -13,6 +16,8 @@ class PostListModel {
 
 // 2. 창고
 class PostListViewModel extends StateNotifier<PostListModel?> {
+  final mContext = navigatorKey.currentContext;
+
   PostListViewModel(super.state, this.ref);
 
   Ref ref;
@@ -23,6 +28,23 @@ class PostListViewModel extends StateNotifier<PostListModel?> {
     ResponseDTO responseDTO = await PostRepository().fetchPostList(sessionUser.jwt!);
 
     state = PostListModel(responseDTO.data);
+  }
+
+  Future<void> notifyAdd(PostSaveReqDTO dto) async {
+    // 통신코드
+    SessionUser sessionUser = ref.read(sessionProvider);
+
+    ResponseDTO responseDTO = await PostRepository().fetchPost(sessionUser.jwt!, dto);
+
+    if (responseDTO.code == 1) {
+      Post newPost = responseDTO.data as Post; // dynamic(Post) -> 다운캐스팅
+      List<Post> newPosts = [newPost, ...state!.posts]; // 기존 상태에 데이터 추가 (전개연산자)
+      state = PostListModel(newPosts); // 창고 데이터 갱신완료 -> watch 구독자는 rebuild됨
+
+      Navigator.pop(mContext!);
+    } else {
+      ScaffoldMessenger.of(mContext!).showSnackBar(SnackBar(content: Text("게시물 작성 실패 : ${responseDTO.msg}")));
+    }
   }
 }
 
